c90ac807c11473706609d7b543fc187b37025e5d
DIBYADEEP RANJAN BANERJEE <95340021+Dibyadeep007@users.noreply.github.com>
2022-07-08 22:02:27 +0530

    My pokeweb

---
+++ index.js
@@ -0,0 +1,8 @@
+let btn=document.querySelector('button');
+btn.addEventListener('click',fn);
+
+function fn()
+{
+  let name=confirm('Are you excited to meet them?');
+//   btn.textContent= name;
+}
